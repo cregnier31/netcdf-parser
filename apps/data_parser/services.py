f2619cce6358197100a6f8d70473719e52542e9a
julien <julien.schiele@gmail.com>
2020-01-08 15:38:30 +0100

    moving kpi dir

--- apps/data_parser/services.py
+++ apps/data_parser/services.py
@@ -102,13 +102,17 @@ def process_files(verbose):
         :return: None
     """
     line = '_________________________________________________________________________________________________________\n'
-    print(line + 'Step 1/4 \t Processing plot files...')
+    print(line + 'Step 1/6 \t Processing plot files...')
     process_plot_files("uploads/plot", verbose)
-    print(line + 'Step 2/4 \t Adding description comment to plots...')
+    print(line + 'Step 2/6 \t Adding description comment to plots...')
     add_summary_to_product('uploads/text', verbose)
-    print(line + 'Step 3/4 \t Processing kpi files...')
-    process_kpi_files("uploads/kpi_INSTAC", verbose)
-    print(line + 'Step 4/4 \t Preload cache files...')
+    print(line + 'Step 3/6 \t Processing insitu kpi files...')
+    process_kpi_files("uploads/kpi/INSITU", verbose)
+    print(line + 'Step 4/6 \t Processing satellite kpi files...')
+    process_kpi_files("uploads/kpi/SAT", verbose)
+    print(line + 'Step 5/6 \t Processing skill score kpi files...')
+    process_kpi_files("uploads/kpi/SKILL_SCORE", verbose)
+    print(line + 'Step 6/6 \t Preload cache files...')
     update_cache()
 
 ###########################################################################################################################################
